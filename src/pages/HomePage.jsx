cdeea0b74c86b713df62bee7a4671729f571c038
Oktay Arslan <oktay16arslan@hotmail.com>
2025-01-19 17:18:50 +0300

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../componenents/Hero'
+import HomeCards from '../componenents/HomeCards'
+import JobListings from '../componenents/JobListings'
+import ViewAllJobs from '../componenents/ViewAllJobs'
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero />
+            <HomeCards />
+            <JobListings />
+            <ViewAllJobs />
+        </>
+    )
+}
+
+export default HomePage
